62a3f8d96023d5059ced963bfec423350daecf04
yfayng1 <yfayng1@students.towson.edu>
2016-04-04 19:14:36 -0400

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404062904) do
+ActiveRecord::Schema.define(version: 20160404230616) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
